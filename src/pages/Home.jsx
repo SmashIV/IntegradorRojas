7049119d8e1c7d8404d5be3b262baa727cf3b6aa
SmashIV <rodrig12q@gmail.com>
2024-10-16 13:41:25 -0500

    Reload issues in vercel fixed :D

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import {useState } from 'react'
 import '../assets/css/home.css'
 import ExpProduc from '../components/ExpProduc'
 import Header from '../components/Header'
